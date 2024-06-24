ccec16cd33cb8a74c5332a92c34f44d4e6ff6bd8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-24 18:11:06 +0900

    🌱feat: invitation api 분리

--- src/services/server/weddingTemplates/weddingTemplates.ts
+++ src/services/server/weddingTemplates/weddingTemplates.ts
@@ -35,15 +35,3 @@ export const getWeddingTemplate = async (templateId: number | string) => {
 
   return response.json();
 };
-
-export const getInvitationProduce = async (produceId: number | string) => {
-  const response = await fetchApi(`/product/invitation/read/${produceId}`, {
-    next: { revalidate: 1000 * 60 * 60 * 24 },
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  return response.json();
-};
